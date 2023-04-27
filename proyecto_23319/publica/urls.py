4135907b09c892343c640cce3f7c48d6c85a22e7
Ary <ariadna.des.garcia@gmail.com>
2023-04-27 10:48:39 -0300

    primer indexen el template

--- proyecto_23319/publica/urls.py
+++ proyecto_23319/publica/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='inicio'),
     path('hola_mundo', views.hola_mundo, name='hola'),
+    path('saludar/<str:nombre>/', views.saludar, name='saludar'),
+    path('proyectos/<int:anio>/<int:mes>/', views.ver_proyecto, name='ver_proyecto'),
+    path('proyectos/<int:anio>/', views.ver_proyecto, name='ver_proyecto'),
+    path('proyectos/', views.ver_proyecto, name='ver_proyecto'),
 ]
--- proyecto_23319/publica/views.py
+++ proyecto_23319/publica/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 def hola_mundo(request):
     return HttpResponse('hola mundo Django')
 def index(request):
-    return HttpResponse(f"""<h1>PROYECTO DJANGO - CODO A CODO</h1>
-                <p>Tes test tes</p>
-            """)
+    if(request.method == 'GET'):
+        titulo="este titulos es accedido por get"
+        
+    else:
+        titulo="este titulos es accedido por post"
+
+    param_1=request.GET.get('param1')
+    return render(request,'publica/index.html',{
+       'titulo':titulo,
+       'param1':param_1,
+       'hoy':datetime.now
+    })
+    #return HttpResponse(f"""<h1>PROYECTO DJANGO - CODO A CODO</h1>
+                #<p>{titulo}</p>
+            #""")
+def saludar(request,nombre):
+    return HttpResponse(f"""
+        <h1>hola {nombre}</h1>  
+        <p>Tes test tes</p>  
+    """)
+def ver_proyecto(request,anio=2023,mes=1):
+    return HttpResponse(f"""
+        <h1>proyectos del mes {mes}/{anio}</h1>  
+        <p>listados de proyectos</p>  
+    """)
